33500eb5b7df2acf529c249495f8eec16af0a296
Oliver Zott <zott_oliver@web.de>
2022-09-19 08:51:53 +0200

    EF Core Setup

---
+++ LeaveManagement.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,37 @@
+﻿using LeaveManagement.Domain;
+using LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Persistence;
+internal class LeaveManagementDbContext : DbContext
+{
+    public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+        {
+            entry.Entity.LastModifiedDate = DateTime.Now;
+            // If entity is not yet in database- set creation date
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.Now;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+}
